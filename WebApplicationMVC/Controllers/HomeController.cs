45b05bcdcc3289fe65a101b7c7e1afb28bafcc82
User <User@DESKTOP-0FQVJVH>
2023-03-25 02:10:07 +0500

    Добавьте файлы проекта.

---
+++ WebApplicationMVC/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplicationMVC.Models;
+
+namespace WebApplicationMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //ViewBag.People = new List<string> { "Tom", "Sam", "Bob" };
+            var people = new List<string> { "Tom", "Sam", "Bob" };
+            var client = new Client { Id = 222, Name = "vladimir"
+        };//создаем клиента
+            return View(client);
+        }
+        //[HttpPost]
+        //public string Index(string username) => $"User Name: {username}";
+        [HttpPost]
+        public string Index(string username, string password, int age, string comment)
+        {
+            return $"User Name: {username}   Password: {password}   Age: {age}  Comment: {comment}";
+        }
+        [HttpPost]
+        public IActionResult Client(string login, string password, int age, string comment)
+        {
+            string authData = $"Login: {login}   Password: {password}   Age: {age}  Comment: {comment}";
+            return Content(authData);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplicationMVC/Models/Client.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationMVC.Models
+{
+    public class Client
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Название")]
+        public string Name { get; set; }
+        public int Summa { get; set; }
+        public int Long { get; set; }
+        public int Procent { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public string Description { get; set; }
+    }
+}
